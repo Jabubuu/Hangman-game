fc8cab48b470448eeaff70bdbabff992a9e022e3
TommiMa <tommi.mant@gmail.com>
2020-10-01 13:30:05 +0300

    testicommit2

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Tommin projekti! :D</Text>
+      <Text>Tommin projekti!!!!! :D</Text>
       <StatusBar style="auto" />
     </View>
   );
