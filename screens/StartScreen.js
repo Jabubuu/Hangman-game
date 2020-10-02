d83b9d7eea85b2a173e8362672a0fc2d4757605e
MarkoPaa <barkcof@gmail.com>
2020-10-02 14:31:24 +0300

    Update StartScreen.js

---
+++ screens/StartScreen.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+import Button from '..components/Button';
+import ScreenStyles from '..styles/ScreenStyles';
+
+
+const StartScreen = (props) => {
+    const[start, setStart]=useState(false);
+
+    const startGame=()=>{
+
+        setStart(true);
+    }
+
+return (
+    <View style={styles.screen}>
+        <Text style={styles.title}>New Game</Text>
+        <Button title="Start" onPress={() => {startGame();}} />
+
+    </View> 
+
+);
+
+}
+
