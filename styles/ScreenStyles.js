e56b8464a5fa35b9f0c862e0d58e4dce1df43ec0
Jabubuu <jabubuu@gmail.com>
2020-10-05 13:01:37 +0300

    Update ScreenStyles.js

---
+++ styles/ScreenStyles.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+
+screen: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  text: {
+    color: 'white',
+  },
+
+});
+
+  export default styles;
